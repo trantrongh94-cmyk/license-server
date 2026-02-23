8cd32ac04704421195c2fae8f440abca808c3c2b
trantrongh94-cmyk <trantrongh94@gmail.com>
2026-02-23 15:16:04 +0700

    ready deploy

---
+++ server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const { Pool } = require("pg");
+
+const app = express();
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("License Server Running 🚀");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started");
+});
